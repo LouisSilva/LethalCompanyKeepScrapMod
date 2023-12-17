fb491741a1e1d4fa2db61d1ff9b21fdce769fad8
Louis <louissilva0899@protonmail.com>
2023-12-17 11:07:29 +0000

    Delete LethalCompanyKeepScrap/obj/Debug/net2.1 directory

--- LethalCompanyKeepScrap/obj/Debug/net2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace LethalCompanyKeepScrap
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "LethalCompanyKeepScrap";
-        public const string PLUGIN_NAME = "LethalCompanyKeepScrap";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
